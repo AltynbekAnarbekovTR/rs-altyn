7556d5df10db54663dca0b12209d4044891eb537
Altynbek312 <altyn_312@mail.ru>
2023-03-20 02:56:20 +0600

    refactor: delete previous task folder

--- module01/src/components/MainPage/MainPage.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import MainPage from './MainPage';
-
-test('Render Cards', () => {
-  render(<MainPage />);
-
-  expect(screen.getAllByRole('listitem').length).toBeGreaterThanOrEqual(1);
-});
